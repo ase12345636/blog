b2990236d361162d78cbf58f2c93647edaa54356
ase12345636 <ase12345636@gmail.com>
2025-11-20 14:49:51 +0800

    Finished format.

--- src/config.ts
+++ src/config.ts
@@ -32,8 +32,8 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		{
-		  src: '/favicon/icon.webp',    // Path of the favicon, relative to the /public directory
-		}
+			src: "/favicon/icon.webp", // Path of the favicon, relative to the /public directory
+		},
 	],
 };
 
