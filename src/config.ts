3ad4eef4c6ad0d91768d3bb8944bc9eb4f687844
ase12345636 <ase12345636@gmail.com>
2025-11-20 14:43:03 +0800

    Finished all setting of blog.

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Terry",
 	bio: "Computer Science, Japan, Research, Miscellany",
 	links: [
---
+++ src/content/posts/first.md
@@ -0,0 +1,10 @@
+---
+title: first
+published: 2025-11-20
+description: 'This is the first post of my blog.'
+image: ''
+tags: [雜記]
+category: '各種雜記'
+draft: true 
+lang: 'zh_TW'
+---
