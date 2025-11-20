a56205633d3328a76c36c9b903f6d0c5de50d960
ase12345636 <ase12345636@gmail.com>
2025-11-20 14:59:43 +0800

    Changes for pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ase12345636.github.io",
-	base: "/base",
+	base: "/blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
